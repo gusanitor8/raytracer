cdd0a9bfe7ebebd0fc463fea6a3567f1e6eb4c95
gusanitor8 <gusanitor8@gmail.com>
2023-08-31 21:56:24 -0600

    linting enabled

--- figures.py
+++ figures.py
@@ -1,14 +1,16 @@
 import numpy as np
 
+
 class Shape(object):
     def __init__(self, position):
         self.position = position
-    
+
     def ray_intersect(self, orig, dir):
         return False
-    
+
+
 class Sphere(Shape):
-    def __init__(self, position, radius):        
+    def __init__(self, position, radius):
         self.radius = radius
         super().__init__(position)
 
@@ -20,7 +22,7 @@ class Sphere(Shape):
 
         if d > self.radius:
             return False
-        
+
         thc = (self.radius**2 - d**2)**0.5
         t0 = tca - thc
         t1 = tca + thc
@@ -29,5 +31,5 @@ class Sphere(Shape):
             t0 = t1
         if t0 < 0:
             return False
-        
+
         return True
--- raytracer.py
+++ raytracer.py
@@ -8,13 +8,14 @@ height = 256
 
 pygame.init()
 
-screen = pygame.display.set_mode((width, height), pygame.DOUBLEBUF | pygame.HWACCEL | pygame.HWSURFACE)
+screen = pygame.display.set_mode(
+    (width, height), pygame.DOUBLEBUF | pygame.HWACCEL | pygame.HWSURFACE)
 screen.set_alpha(None)
 
 raytracer = RayTracer(screen)
 raytracer.rtClearColor(0.5, 0.5, 0.5)
 
-raytracer.scene.append(Sphere( position = (0,0,-5), radius = 1 ))
+raytracer.scene.append(Sphere(position=(0, 0, -5), radius=1))
 
 
 isRunning = True
@@ -23,13 +24,12 @@ while isRunning:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             isRunning = False
-            
+
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 isRunning = False
-    
 
-    raytracer.rtClear()    
+    raytracer.rtClear()
     raytracer.rtRender()
     pygame.display.flip()
 
